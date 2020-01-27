6dc3c5f65035f8b0d74865200caeffd06a5244a2
hw <HP@huwei.gzed.inside>
2020-01-28 01:49:03 +0800

    axios抽离

--- day11/learnaxios/src/network/request.js
+++ day11/learnaxios/src/network/request.js
@@ -46,5 +46,26 @@ export function request(config) {
     baseURL: 'http://123.207.32.32:8000',
     timeout: 5000
   })
+  //axios的拦截器
+  //axios设置请求拦截器
+  instance1.interceptors.request.use(config => {
+    //请求拦截器的作用
+    //1.比如config中的一些信息不符合服务器的要求
+    //2.比如每次发送网络请求时，都希望在界面中显示一个请求的图标
+    //3.某些网络请求（比如登录（token)），必须携带一些特殊的信息
+    config.headers.mytoken = 'nihao' //设置响应头
+    return config
+  }, err => {
+    console.log(err)
+  })
+
+  //axios设置响应拦截器
+  instance1.interceptors.response.use(response => {
+    return response.data //拦截处理响应结果，直接返回需要的数据
+  }, err => {
+    console.log(err)
+  })
+
+
   return instance1(config)
 }
